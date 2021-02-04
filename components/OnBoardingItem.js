cf739d3026f464b9eeeb648f3d1eb20901d732d6
omeryaldirgan <omeryaldirgan@hotmail.com>
2021-02-04 22:51:39 +0300

    Create OnBoardingItem.js

---
+++ components/OnBoardingItem.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View ,FlatList} from 'react-native';
+
+const OnboardingItem=()=>{
+   return (
+      <View style={styles.container}>
+         <FlatList/>
+      </View>
+   );
+}
+export default OnboardingItem;
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
+});
